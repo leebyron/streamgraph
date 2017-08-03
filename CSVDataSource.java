e7370a65f95dc50eb190c1cef7ae4d89e42faa5c
Lee Byron <lee@leebyron.com>
2017-08-03 16:01:11 -0700

    Squashed commit of the following:
    commit 5c63aa1bf385167e1b8ceceab511e8b8a0d8763b
    Author: Lee Byron <lee@leebyron.com>
    Date:   Thu Aug 3 15:17:57 2017 -0700
    
        tweaks
    
    commit 440356adc6b5062e9afdf53a8481900f5d94aa40
    Author: Albert Sun <albert@albertsun.info>
    Date:   Wed Nov 10 10:20:39 2010 -0500
    
        fixed setting of numLayers and layerSize to work at proper scope
    
    commit 1144ccc6991d086a496b76e17edab5bd9be6d455
    Author: SunA <suna@Macintosh-d49a20ff985f.local>
    Date:   Tue Nov 9 18:25:36 2010 -0500
    
        renamed to CSVDataSource.java
    
    commit d863e32c469a5cbf710784a77e433f5953f098ae
    Author: SunA <suna@Macintosh-d49a20ff985f.local>
    Date:   Tue Nov 9 18:13:44 2010 -0500
    
        diff layout
    
    commit ac087b432d98fd94333e74eb34cc7c649673ac27
    Author: SunA <suna@Macintosh-d49a20ff985f.local>
    Date:   Tue Nov 9 17:33:23 2010 -0500
    
        fixed TweetDataSource
    
    commit bd2b2ae3984e667e158c51b1906fb6a7b77480f6
    Author: SunA <suna@Macintosh-d49a20ff985f.local>
    Date:   Tue Nov 9 16:54:40 2010 -0500
    
        added new data source, csv

---
+++ CSVDataSource.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import processing.core.*;
+
+/**
+ * CSVDataSource
+ * Read data from a CSV file.
+ * Each Layer corresponds to one line, with the first entry in the line the name of the layer.
+ * Assumes every line is the same length.
+ * Ignores the parameters given to make.
+ *
+ * @author Albert Sun
+ * @author Lee Byron
+ * @author Martin Wattenberg
+ */
+public class CSVDataSource implements DataSource {
+
+  public String[] data;
+
+  public CSVDataSource(PApplet parent, String filename) {
+    data = parent.loadStrings(filename);
+  }
+
+  public Layer[] make(int a, int b) {
+    int numLayers = data.length;
+    Layer[] layers = new Layer[numLayers];
+
+    for (int i = 0; i < numLayers; i++) {
+      String[] fields  = data[i].split(",");
+      int sizeArrayLength  = fields.length - 1;
+      String name      = fields[0];
+      size             = makeDataArray(fields);
+      layers[i]        = new Layer(name, size);
+    }
+
+    return layers;
+  }
+
+  protected float[] makeDataArray(String[] a) {
+    float[] x = new float[a.length - 1];
+    for (int i = 1; i < a.length; i++) {
+      x[i-1] = Float.valueOf(a[i].trim()).floatValue();
+    }
+    return x;
+  }
+
+}
